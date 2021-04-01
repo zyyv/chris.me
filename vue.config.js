75a1272c5c90bb87332d09ab789b52abf7c926d7
chriszhu <1633711653@qq.com>
2021-04-01 18:00:14 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:1324',
+        target: 'http://localhost:2430',
         changeOrigin: true,
         ws: true,
         pathRewrite: {
