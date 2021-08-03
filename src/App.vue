865dcda01503fa9ed4406783359597a0327b9eb1
CHRISZHU\Chris <1633711653@qq.com>
2021-08-03 18:54:01 +0800

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,27 @@
 <template>
-  <div id="app" :class="model?'light':'night'">
-    <el-container class="container" style>
-      <el-aside class="aside" :width="openAside ? '38%' : '22%'">
-        <Info />
-      </el-aside>
-      <el-main class="main">
-        <router-view />
-      </el-main>
-    </el-container>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
-<script>
-import Info from '@/components/Info'
-import { mapState } from "vuex";
-export default {
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
   name: 'App',
-  components: { Info },
-  data () {
-    return {}
-  },
-  created () {
-    localStorage.setItem('ip', window.returnCitySN['cip'])
-    localStorage.setItem('cityname', window.returnCitySN['cname'])
-    this.$store.state.user.token || this.$store.dispatch('user/getTourist')
-  },
-  computed: {
-    ...mapState('menu', ['openAside']),
-    ...mapState('user', ['model']),
+  components: {
+    HelloWorld
   }
-}
+})
 </script>
-<style lang="scss" scoped>
-$bg-color: #fafbff;
-$color: #555;
-$night-bg-color: #151518;
-$night-color: #999;
+
+<style>
 #app {
-  .container {
-    height: 100vh;
-  }
-  .aside {
-    transition: all 1s;
-    background-image: url('http://qfr5vuqoo.hn-bkt.clouddn.com/4k_1.jpg');
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center center;
-    color: rgba(255, 255, 255, 0.8);
-  }
-  &.night{
-    .main{
-      background-color: $night-bg-color !important;
-      color: $night-color !important;
-    }
-  }
-  .main {
-    transition: all 1s;
-    background-color: $bg-color;
-    color: $color;
-    padding: 0;
-    overflow: hidden;
-  }
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
