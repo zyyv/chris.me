b183b849959f1c256c3a724041d3f0cc9d64e55e
chris <1633711653@qq.com>
2021-09-15 09:18:17 +0800

    init new branch

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+const pathResolve = (src: string) => resolve(__dirname, src)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': pathResolve('src'),
+      '@c': pathResolve('src/components'),
+      '@v': pathResolve('src/views')
+    }
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:2430',
+        changeOrigin: true
+      }
+    }
+  }
 })
