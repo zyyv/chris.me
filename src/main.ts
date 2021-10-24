0d5721c3abd7f7c80d186b8c0cb7b6b54b7f1c39
CHRISZHU\Chris <1633711653@qq.com>
2021-10-24 21:51:15 +0800

    卸载 vuex

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
-import { store, key } from '@/store'
 import { HttpRequest } from './plugins'
 import '@/styles/index.scss'
 
 const app = createApp(App)
 app.use(router)
-app.use(store, key)
 app.use(HttpRequest())
 app.mount('#app')
