f853fe9ddcae804b5988afdc43f5f61d443af65e
chris <1633711653@qq.com>
2021-10-29 16:52:47 +0800

    remove tailwind and test add unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
 import { resolve } from 'path'
 
 const pathResolve = (src: string) => resolve(__dirname, src)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({ script: { refSugar: true } })],
+  plugins: [vue({ script: { refSugar: true } }), Unocss()],
   resolve: {
     alias: {
       '@': pathResolve('src'),
