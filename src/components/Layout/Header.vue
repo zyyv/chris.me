e2ed26dd20380865b55ed6e28c794e17599d9d45
chris <1633711653@qq.com>
2021-10-29 16:34:04 +0800

    update header

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -10,8 +10,38 @@
     <nav class="nav">
       <div class="spacer"></div>
       <div class="right">
-        <router-link to="/posts"> Blog </router-link>
-        <router-link to="/share"> Share </router-link>
+        <router-link to="/posts">
+          <svg
+            class="inline"
+            width="1.2em"
+            height="1.2em"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+            data-v-939756fc=""
+            style="vertical-align: sub"
+          >
+            <path
+              d="M20 22H4a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1zm-1-2V4H5v16h14zM7 6h4v4H7V6zm0 6h10v2H7v-2zm0 4h10v2H7v-2zm6-9h4v2h-4V7z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </router-link>
+        <router-link to="/share">
+          <svg
+            class="inline"
+            width="1.2em"
+            height="1.2em"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+            data-v-939756fc=""
+            style="vertical-align: sub"
+          >
+            <path
+              d="M9.973 18H11v-5h2v5h1.027c.132-1.202.745-2.194 1.74-3.277c.113-.122.832-.867.917-.973a6 6 0 1 0-9.37-.002c.086.107.807.853.918.974c.996 1.084 1.609 2.076 1.741 3.278zM10 20v1h4v-1h-4zm-4.246-5a8 8 0 1 1 12.49.002C17.624 15.774 16 17 16 18.5V21a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2v-2.5C8 17 6.375 15.774 5.754 15z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </router-link>
         <router-link to="/projects"> Projects </router-link>
         <router-link to="/bookmarks">
           <svg
