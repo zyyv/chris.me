dafa05a238773dc98813fbca361bb983e11dd9a5
CHRISZHU\Chris <1633711653@qq.com>
2021-11-16 22:41:13 +0800

    add some plugins but it's not complete

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { createApp } from 'vue'
 import { createI18n } from 'vue-i18n'
+import { createRouter } from 'vue-router'
+import generatedRoutes from 'virtual:generated-pages'
+import { setupLayouts } from 'virtual:generated-layouts'
 import App from './App.vue'
-import router from './router'
 import { pinia } from './store'
 import { httpRequest, vDirective } from './plugins'
 import 'uno.css'
 import '@/styles/index.scss'
 
 const app = createApp(App)
-app.use(router)
+app.use(createRouter({ routes: setupLayouts(generatedRoutes) }))
 app.use(pinia)
 app.use(createI18n({}))
 app.use(httpRequest())
--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,43 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import Pages from 'vite-plugin-pages'
+import Layouts from 'vite-plugin-vue-layouts'
+import Markdown from 'vite-plugin-md'
 
 const pathResolve = (src: string) => resolve(__dirname, src)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({ script: { refSugar: true } }),
+    Vue({ script: { refSugar: true }, include: [/\.vue$/, /\.md$/] }),
     Unocss(),
+    Pages({ extensions: ['vue', 'md'] }),
+    Layouts(),
     AutoImport({
       imports: ['vue', 'pinia', 'vue-router', 'vue-i18n', '@vueuse/core'],
       dts: 'src/auto-imports.d.ts'
     }),
     Components({
+      extensions: ['vue', 'md'],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
       dts: 'src/components.d.ts'
     }),
     vueI18n({
       runtimeOnly: true,
       compositionOnly: true,
       include: [pathResolve('locales/**')]
-    })
+    }),
+    Markdown()
   ],
   resolve: {
     alias: {
       '@': pathResolve('src'),
       '@a': pathResolve('src/assets'),
-      '@c': pathResolve('src/components'),
-      '@v': pathResolve('src/views'),
       '@p': pathResolve('src/plugins'),
       '@u': pathResolve('src/use')
     }
