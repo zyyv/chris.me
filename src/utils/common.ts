36ba80abd62f816343be858e2b4c969d5098fe82
CHRISZHU\Chris <1633711653@qq.com>
2021-11-22 00:05:22 +0800

    add common util func

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -11,3 +11,24 @@ export const addUnit = (val: any) => {
 }
 
 export const inBrowser = typeof window !== 'undefined'
+
+export const cloneDeep: any = (value: any) => {
+  if (Array.isArray(value))
+    return value.map(child => cloneDeep(child))
+
+  if (typeof value === 'object' && value !== null)
+    return Object.fromEntries(Object.entries(value).map(([k, v]) => [k, cloneDeep(v)]))
+
+  return value
+}
+
+export const cloneNodes = (nodes: any[], source: any) => {
+  return nodes.map((node) => {
+    const cloned = node.clone()
+
+    if (source !== undefined)
+      cloned.source = source
+
+    return cloned
+  })
+}
