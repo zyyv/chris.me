837100f6843e95e1c066f6b25fcc67c5191da655
CHRISZHU\Chris <1633711653@qq.com>
2021-11-23 22:20:52 +0800

    style: remove scss

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import App from './App.vue'
 import { createCtx } from './use'
 import 'uno.css'
-import '@/styles/index.scss'
+import '@/styles/index.css'
 
 createCtx(App, (ctx) => {
   Object.values(import.meta.globEager('./modules/*.ts')).map(i => i.install?.(ctx))
