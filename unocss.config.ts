912f5138b27c0d694558f92811135dc421a88090
chris-zhu <1633711653@qq.com>
2022-02-08 14:32:28 +0800

    fix: complete unocss config

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,35 +1,52 @@
-import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss'
 
-export const createConfig = ({ strict = true, dev = true } = {}) => {
-  return defineConfig({
-    envMode: dev ? 'dev' : 'build',
-    theme: {
-      fontFamily: {
-        sans: '\'Inter\', sans-serif',
-        mono: '\'Fira Code\', monospace'
+import { defineConfig, presetAttributify, presetIcons, presetUno, presetWebFonts } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetAttributify(),
+    presetUno(),
+    presetIcons({
+      collections: {
+        'logos': () => import('@iconify-json/logos/icons.json').then(i => i.default as any),
+        'carbon': () => import('@iconify-json/carbon/icons.json').then(i => i.default as any),
+        'akar-icons': () => import('@iconify-json/akar-icons/icons.json').then(i => i.default as any),
+        'ri': () => import('@iconify-json/ri').then(i => i.icons as any)
       }
-    },
-    presets: [
-      presetAttributify({ strict }),
-      presetUno(),
-      presetIcons({
-        collections: {
-          carbon: () => import('@iconify-json/carbon').then(i => i.icons as any),
-          ri: () => import('@iconify-json/ri').then(i => i.icons as any)
-          // mdi: () => import('@iconify-json/mdi/icons.json').then(i => i.default as any),
-          // logos: () => import('@iconify-json/logos/icons.json').then(i => i.default as any),
-          // twemoji: () => import('@iconify-json/twemoji/icons.json').then(i => i.default as any),
-          // ri: () => import('@iconify-json/ri/icons.json').then(i => i.default as any),
-          // tabler: () => import('@iconify-json/tabler/icons.json').then(i => i.default as any),
-          // uim: () => import('@iconify-json/uim/icons.json').then(i => i.default as any),
-        }
-      })
-    ],
-    rules: [
-      ['p-c', { position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }],
-      ['f-c', { 'display': 'flex', 'justify-content': 'center', 'align-items': 'center' }]
-    ]
-  })
-}
+    }),
+    presetWebFonts({
+      fonts: {
+        // these will extend the default theme
+        sans: 'Roboto',
+        mono: ['Fira Code', 'Fira Mono:400,700'],
+        // custom ones
+        lobster: 'Lobster',
+        lato: [
+          {
+            name: 'Lato',
+            weights: ['400', '700'],
+            italic: true
+          },
+          {
+            name: 'sans-serif',
+            provider: 'none'
+          }
+        ]
+      }
+    })
+  ],
+  layers: {
+    default: 1,
+    modele: 2
+  },
+  rules: [
+    ['pr', { position: 'relative' }, { layer: 'modele' }],
+    ['pa', { position: 'absolute' }, { layer: 'modele' }]
+  ],
+  shortcuts: [
+    ['f-c', 'flex justify-center items-center'],
+    ['f-c-c', 'f-c flex-col'],
+    ['p-c', 'pa top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'],
+    ['icon-btn', 'w-5.5 h-5.5 cursor-pointer select-none transition-duration-200 ease-in-out hover:text-teal-600 dark:text-[#afbac6] dark:hover:text-[#d9dfe9]']
+  ]
+})
 
-export default createConfig()
