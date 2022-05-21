94ca09c1e1b63fd7f1acdcd8d4247aa6993146b1
chris-zhu <1633711653@qq.com>
2022-05-21 14:11:08 +0800

    chore: update seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,15 @@ export default defineNuxtConfig({
         rel: 'icon', type: 'image/x-icon', href: '/favicon.ico',
       },
     ],
+    meta: [
+      { hid: 'description', name: 'description', content: 'Chirs blog is a place to share my thoughts and learn new things.' },
+      { hid: 'keywords', name: 'keywords', content: 'chris, blog, thoughts, learn, new, things' },
+      { 'http-equiv': 'pragma', 'content': 'no-cache' },
+      { 'http-equiv': 'cache-control', 'content': 'no-cache' },
+      { 'http-equiv': 'expires', 'content': '0' },
+    ],
+    charset: 'utf-8',
+    viewport: 'width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0',
   },
   buildModules: [
     '@vueuse/nuxt',
