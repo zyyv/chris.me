733c79987f247d42f09dfd3194e7e4f21b83ffed
Chris <1633711653@qq.com>
2022-10-17 14:23:37 +0800

    chore: update

---
+++ components/content/ArticleHeader.vue
@@ -0,0 +1,18 @@
+<script lang='ts' setup>
+import { Article } from '~/types'
+import data from '~/data/posts.json'
+
+const props = defineProps<{
+  title: string
+}>()
+
+const article = computed(() => (data as Article[]).find(i => i.slug === props.title))
+const timeAgo = useTimeAgo(new Date(article.value?.ctimeMs!)).value.replace('"', '')
+</script>
+
+<template>
+  <div>
+    <h1>{{ article?.slug }}</h1>
+    <p>{{ article?.ctime }} <sub bottom-0>{{ timeAgo }}</sub></p>
+  </div>
+</template>
