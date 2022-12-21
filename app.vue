2c15dcf5935fff6cc9e780e4d133a0ca63d82128
Chris <1633711653@qq.com>
2022-12-21 14:04:57 +0800

    pref: update loading height & color

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <NuxtLoadingIndicator color="repeating-linear-gradient(to right,rgba(192,132,252,0.5) 0%,rgba(192,132,252,0.8) 100%)" />
+  <NuxtLoadingIndicator :height="1" color="repeating-linear-gradient(to right,rgba(192,132,252,1) 0%,rgba(248,113,113,1) 50%,rgba(45,212,191,1) 100%)" />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
