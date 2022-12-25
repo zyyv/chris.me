1d5d3b807eceec1af018c66bb2a03378ed6e8cd3
Chris <1633711653@qq.com>
2022-12-25 15:23:21 +0800

    chore: update

--- server/constants/index.ts
+++ server/constants/index.ts
@@ -1,3 +1,3 @@
 import { Octokit } from 'octokit'
-export const TOKEN = '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'
+export const TOKEN = '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'
 export const OctokitCtx = new Octokit({ auth: TOKEN })
