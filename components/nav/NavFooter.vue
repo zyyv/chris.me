4a849f377e93f1e51e8998e9f3ff8f18da6a408c
Chris <hizyyv@gmail.com>
2023-06-15 09:31:34 +0800

    chore: update footer

--- components/nav/NavFooter.vue
+++ components/nav/NavFooter.vue
@@ -7,6 +7,7 @@
     ma
   >
     <div text="xs gray4" flex justify-center gap-3>
+      <a href="https://beian.miit.gov.cn/" target="_blank">蜀ICP备2022005364号</a>
       <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a>2023-PRESENT © Chris
     </div>
     <i i-fluent-emoji:ghost />
