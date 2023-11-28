71cb73aa5b40c6aef9c0670d19e8223fc66be3e0
Chris <hizyyv@gmail.com>
2023-11-28 17:53:49 +0800

    chore: eslint upgrade

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {},
+  {
+    ignores: [
+      'dist',
+      'node_modules',
+      '.output',
+      '.nuxt',
+      'content/posts',
+      'data',
+    ],
+  },
+  {
+    rules: {
+      "n/prefer-global/process": "off"
+    },
+  },
+  {
+    files: [
+      '*.d.ts',
+    ],
+    rules: {
+      'unused-imports/no-unused-vars': 'off',
+      'eslint-comments/no-unlimited-disable': 'off',
+    },
+  },
+)
