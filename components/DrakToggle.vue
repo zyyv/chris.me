1c9333e8c4faa5f79a265a1affb73cf017951e5a
Chris <hizyyv@gmail.com>
2024-05-15 17:05:12 +0800

    fix: move css into component

--- components/DrakToggle.vue
+++ components/DrakToggle.vue
@@ -1,8 +1,81 @@
+<script setup lang="ts">
+const isDark = useDark()
+
+/**
+ * Credit to [@hooray](https://github.com/hooray)
+ * @see https://github.com/vuejs/vitepress/pull/2347
+ */
+function toggleDark(event: MouseEvent) {
+  const isAppearanceTransition = document.startViewTransition
+    && !window.matchMedia('(prefers-reduced-motion: reduce)').matches
+
+  if (!isAppearanceTransition) {
+    isDark.value = !isDark.value
+    return
+  }
+
+  const x = event.clientX
+  const y = event.clientY
+  const endRadius = Math.hypot(
+    Math.max(x, innerWidth - x),
+    Math.max(y, innerHeight - y),
+  )
+  // @ts-expect-error: Transition API
+  const transition = document.startViewTransition(async () => {
+    isDark.value = !isDark.value
+    await nextTick()
+  })
+  transition.ready
+    .then(() => {
+      const clipPath = [
+        `circle(0px at ${x}px ${y}px)`,
+        `circle(${endRadius}px at ${x}px ${y}px)`,
+      ]
+      document.documentElement.animate(
+        {
+          clipPath: isDark.value
+            ? [...clipPath].reverse()
+            : clipPath,
+        },
+        {
+          duration: 400,
+          easing: 'ease-out',
+          pseudoElement: isDark.value
+            ? '::view-transition-old(root)'
+            : '::view-transition-new(root)',
+        },
+      )
+    })
+}
+</script>
+
 <template>
-  <div
-    :title="isDark ? 'Switch to light mode' : 'Switch to dark mode'" icon-btn select-none
-    @click="toggleDark"
-  >
-    <i dark:i-akar-icons:moon-fill i-carbon:light-filled />
-  </div>
+  <ClientOnly>
+    <div
+      :title="isDark ? 'Switch to light mode' : 'Switch to dark mode'" icon-btn select-none
+      @click="toggleDark"
+    >
+      <i dark:i-akar-icons:moon-fill i-carbon:light-filled />
+    </div>
+  </ClientOnly>
 </template>
+
+<style scoped>
+::view-transition-old(root),
+::view-transition-new(root) {
+  animation: none;
+  mix-blend-mode: normal;
+}
+::view-transition-old(root) {
+  z-index: 1;
+}
+::view-transition-new(root) {
+  z-index: 9999;
+}
+.dark::view-transition-old(root) {
+  z-index: 9999;
+}
+.dark::view-transition-new(root) {
+  z-index: 1;
+}
+</style>
