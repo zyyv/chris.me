985448d5dd006da7412a0094eff19d0d532cefd1
Chris <hizyyv@gmail.com>
2024-09-26 17:57:43 +0800

    chore: update

--- server/api/contribution.ts
+++ server/api/contribution.ts
@@ -1,11 +1,26 @@
-import type { ContributeData } from '~/types'
+import { useOctokit } from '../utils/github'
 
 export default defineEventHandler(async (event) => {
-  if (import.meta.dev)
-    return (await import('~/mock/contributions.json')).default as ContributeData
+  // if (import.meta.dev)
+  //   return (await import('~/mock/contributions.json')).default as ContributeData
 
   const { name, year } = getQuery(event)
-  const API = `https://github-contributions-api.jogruber.de/v4/${name}?y=${year}`
+  const octokit = useOctokit()
+  try {
+    const response = await octokit.request('GET /users/{username}/events', {
+      username: name as string,
+      per_page: 100,
+    })
 
-  return $fetch<ContributeData>(API)
+    const contributions = response.data.filter((d) => {
+      const eventYear = new Date(d.created_at!).getFullYear()
+      return eventYear === Number.parseInt(year as string, 10)
+    })
+
+    return contributions
+  }
+  catch (error) {
+    console.error('Error fetching contributions:', error)
+    throw error
+  }
 })
