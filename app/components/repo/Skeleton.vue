aa60827b91230537f40046f5186008bbc115c027
Chris <hizyyv@gmail.com>
2024-12-03 22:58:35 +0800

    perf: optimize skeleton color

--- app/components/repo/Skeleton.vue
+++ app/components/repo/Skeleton.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="border rounded-lg p-4 animate-pulse">
-    <div class="h-6 w-2/3 bg-gray-200 rounded mb-2" />
-    <div class="h-4 w-4/5 bg-gray-200 rounded mb-4" />
+  <div class="rounded-lg p-4 animate-pulse" dark:o-gray-300 o-gray-500 b="~ context:20" flex="~ col" gap-3>
+    <div fsc gap-2 w-full>
+      <div class="h-5 w-1/3 bg-context:40 rounded" />
+      <div class="bg-context:40 rounded-full h-4 w-1/10" />
+    </div>
+    <div class="h-3 w-4/5 bg-context:40 rounded" />
     <div class="flex items-center space-x-4">
-      <div class="h-4 w-16 bg-gray-200 rounded" />
-      <div class="h-4 w-16 bg-gray-200 rounded" />
+      <div class="h-4 w-16 bg-context:40 rounded" />
+      <div class="h-4 w-16 bg-context:40 rounded" />
     </div>
   </div>
 </template>
