1778a7e88ab4c4e3f302af3995f2e33190a9d2b7
Chris <hizyyv@gmail.com>
2024-12-04 17:39:25 +0800

    fix: adjust projects repo

--- server/api/repos.ts
+++ server/api/repos.ts
@@ -2,32 +2,30 @@ import type { Repo } from '~/types'
 import { useOctokit } from '../utils/github'
 
 export default defineEventHandler(async () => {
-  const { data } = await useOctokit().request('GET /user/repos', {
-    per_page: 100,
-    type: 'owner',
-    sort: 'updated',
-  })
+  const { data } = await useOctokit().request('GET /user/repos', { per_page: 100 })
 
-  const publicRepos = data.filter(repo => !repo.private && !repo.archived)
+  const publicRepos = data.filter(repo => !repo.fork && !repo.archived && !repo.private && repo.permissions?.admin && repo.description)
   const publicAndNotForkRepos = publicRepos.filter(repo => !repo.fork)
 
   const repoGroups: Record<string, Repo[]> = {
+    'UI': filterRepos(publicRepos, 'ui'),
+    'UnoCSS': filterRepos(publicRepos, 'unocss').filter(repo => !repo.topics?.includes('unocss-community')),
+    'UnoCSS Community': filterRepos(publicRepos, 'unocss-community'),
     'Vite Ecosystem': filterRepos(publicAndNotForkRepos, 'vite'),
     'Plugins': filterRepos(publicAndNotForkRepos, 'plugin'),
-    'Presets': filterRepos(publicAndNotForkRepos, 'preset'),
     'Utils': filterRepos(publicAndNotForkRepos, 'util'),
-    'UnoCSS': filterRepos(publicRepos, 'unocss'),
-    'UI': filterRepos(publicRepos, 'ui'),
     'Config': filterRepos(publicAndNotForkRepos, 'config'),
     'Component': filterRepos(publicAndNotForkRepos, 'component'),
     'Templates': filterRepos(publicAndNotForkRepos, 'template'),
     'Games': filterRepos(publicAndNotForkRepos, 'game'),
-    'All': publicAndNotForkRepos,
+    'Me': filterRepos(publicAndNotForkRepos, 'me'),
   }
 
-  return repoGroups
+  return Object.fromEntries(Object.entries(repoGroups).filter(([_, repos]) => repos.length > 0))
 })
 
 function filterRepos(repos: Repo[], key: string) {
-  return repos.filter(repo => repo.topics && repo.topics.includes(key))
+  return repos.filter(repo => repo.topics && repo.topics.includes(key)).sort((a, b) => {
+    return a.stargazers_count > b.stargazers_count ? -1 : 1
+  })
 }
