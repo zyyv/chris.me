f97cda46f92e8f4f06759a0e7082566443745566
Chris <hizyyv@gmail.com>
2024-12-05 17:40:16 +0800

    chore: test

--- app/components/content/ProseH3.vue
+++ app/components/content/ProseH3.vue
@@ -1,9 +1,36 @@
 <script setup lang="ts">
-defineProps<{ id?: string }>()
+import { computed, useRuntimeConfig } from '#imports'
+import { proseHeadingClick } from '~/utils'
+
+const props = defineProps<{
+  id?: string
+}>()
+
+const { headings } = useRuntimeConfig().public.mdc
+const generate = computed(() => props.id && (
+  (typeof headings?.anchorLinks === 'boolean' && headings?.anchorLinks === true)
+  || (typeof headings?.anchorLinks === 'object' && headings?.anchorLinks?.h3)),
+)
 </script>
 
 <template>
-  <ProseHeading :id tag="h3">
-    <slot />
-  </ProseHeading>
+  <h3 :id class="pr group text text-op-90">
+    <a
+      v-if="props.id && generate"
+      :href="`#${props.id}`"
+      text-inherit
+      @click="proseHeadingClick($event, props.id)"
+    >
+      <slot />
+    </a>
+    <slot v-else />
+    <span
+      v-if="props.id && generate"
+      pa fcc size-5.5 bg-primary:10 rd pyc left--7
+      group-hover:op-100 trans op-0 cursor-pointer
+      @click="proseHeadingClick($event, props.id)"
+    >
+      <i i-carbon:link text-sm />
+    </span>
+  </h3>
 </template>
